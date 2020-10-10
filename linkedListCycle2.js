393b1241f8a75865eed833ad632f0be76d41b27c
Jason Maldonado <platocrat@tuta.io>
2020-10-09 21:06:45 -0500

    added floyd's tortoise and hare algorithm

--- linkedListCycle2.js
+++ linkedListCycle2.js
@@ -50,7 +50,7 @@
  *                     the linked list, which takes O(n) space.
  */
 function detectCycle(head) {
-  visited = new Set() // keep track of all `visited` nodes
+  let visited = new Set() // keep track of all `visited` nodes
   /**
    * Each node of the linked list will be traversed by calling the `.next` 
    * method on the instance of the `LinkNode` function, which is represented by
@@ -61,7 +61,7 @@ function detectCycle(head) {
    * `.next` method) until we find the starting point (duplicate node) of the 
    * cycle.
    */
-  node = head
+  let node = head
 
   while (node != null) {
     if (visited.has(node)) {
@@ -73,4 +73,90 @@ function detectCycle(head) {
       node = node.next
     }
   }
+
+  return null
+}
+
+/**
+ * @dev
+ * Approach 2: Floyd's Tortoise and Hare Algorithm
+ *
+ * Intuition:          The idea behind the algorithm is, if you have two
+ *                     pointers in a linked list, one moving twice as fast (the
+ *                     hare) than the other (the tortoise), then if they
+ *                     intersect, there is a cycle in the linked list. If the
+ *                     hare and tortoise don't intersect, then there is no
+ *                     cycle.
+ *
+ *                     In the problem, we're asked to return a boolean for
+ *                     whether or not there is a cycle. You are given a linked
+ *                     list, `LinkNode`, with each node's value that is
+ *                     represented by the `.val` method, and a pointer (which
+ *                     points to the next node after each node value) that is
+ *                     represented by the `.next` method.
+ *
+ * Time complexity:    O()
+ *
+ * Space complexity:   O()
+ *
+ * @param {Number[]} head
+ * @return {Number} node
+ */
+function detectCycle(head) {
+  function getIntersect(head) {
+    /**
+     * Initiate slow and fast pointer, `tortoise` and `hare`, respectively, each
+     * starting at the `head` of the linked list `LinkNode`.
+     */
+    let tortoise = head
+    let hare = head
+
+    /**
+     * A fast pointer will either loop around a cycle and meet the slow pointer,
+     * or reach the `null` at the end of a non-cyclic list.
+     */
+    while (hare && hare.next) {
+      tortoise = tortoise.next
+      hare = hare.next.next
+
+      if (tortoise == hare) {
+        return tortoise
+      }
+    }
+
+    /**
+     * If we process the entire linked list and haven't found a meeting node, 
+     * return null
+     */
+    return null
+  }
+
+  if (head == null) {
+    return null
+  }
+
+  /**
+   * If there is a cycle, the fast/slow pointers will intersect at some node.
+   * Otherwise, there is no cycle, so we cannot find an entrance to a cycle.
+   */
+  intersection = getIntersect(head)
+
+  if (intersection == null) {
+    return null
+  }
+
+  /**
+   * To find the entrance to the cycle, we have two pointers traverse at the
+   * same speed -- one from the front of the list, and the other from the point
+   * of intersection.
+   */
+  pointer1 = head
+  pointer2 = intersection
+
+  while (pointer1 != pointer2) {
+    pointer1 = pointer1.next
+    pointer2 = pointer2.next
+  }
+
+  return pointer1
 }
