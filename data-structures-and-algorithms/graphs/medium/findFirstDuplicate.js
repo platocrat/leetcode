67f2b01b5ff0dc7e4f19111269f0969cf86ba182
Jason Maldonado <platocrat@tuta.io>
2020-10-15 12:14:17 -0500

    Added maxPrefixSuffixSum.js

--- data-structures-and-algorithms/graphs/medium/findFirstDuplicate.js
+++ data-structures-and-algorithms/graphs/medium/findFirstDuplicate.js
@@ -2,45 +2,41 @@
  * @dev 
  * 287. Find the Duplicate Number
  * 
- * Difficulty:         Medium
+ * Difficulty: Medium
  * 
  * Reference:
  * https://leetcode.com/problems/find-the-duplicate-number/solution/
  * 
- * 
  * Approach 1: Sorting
  * 
- * Intuition:          This problem is provably solvable using the pigeonhole 
- *                     principle: if n items are put into m containers, with
- *                     n > m, then at least one container must contain more than
- *                     one item.
+ * Intuition:         
+ * This problem is provably solvable using the pigeonhole principle: if `n` 
+ * items are put into m containers, with `n > m`, then at least one container 
+ * must contain more than one item.
  * 
- * Time Complexity:    O(nlogn)
- 
- *                     The `sort` invocation costs O(nlogn) time, so it 
- *                     dominates the linear scan of the for loop.
+ * Time Complexity:   O(nlogn),
+ * The `sort` invocation costs O(nlogn) time, so it dominates the linear scan of
+ * the for loop.
  * 
- * Space Complexity:   O(1) if an in-place algorithm is used, or O(n) if not
+ * Space Complexity:  O(1), if an in-place algorithm is used, or O(n) if not
+ * We sort `numbers` in place, so the memory footprint is constant. If we cannot
+ * modify the input array, then we must allocate linear space for a copy of 
+ * `numbers` and sort that instead.
  * 
- *                     We sort `numbers` in place, so the memory footprint is 
- *                     constant. If we cannot modify the input array, then we 
- *                     must allocate linear space for a copy of `numbers` and
- *                     sort that instead.
- * 
- * @param {Number[]} _number
+ * @param {Number[]} numbers
  * @return {Number}
  */
-function findDuplicate(_numbers) {
+function findDuplicate1(numbers) {
   /**
    * Because we know there is exactly one duplicated element in the array,
    * we know in a sorted array any duplicate will be appear adjacent to its 
    * first occurrence, so we should return the duplicate as soon as its found.
    */
-  _numbers.sort() // sort the array -- O(nlogn) time
+  numbers.sort() // sort the array -- O(nlogn) time
 
-  for (let i in _numbers) {
-    if (_numbers[ i ] == _numbers[ i - 1 ]) {
-      return _numbers[ i ] // return duplicate
+  for (let i in numbers) {
+    if (numbers[ i ] == numbers[ i - 1 ]) {
+      return numbers[ i ] // return duplicate
     }
   }
 }
@@ -49,42 +45,37 @@ function findDuplicate(_numbers) {
  * @dev
  * Approach 2: Set
  *
- * Intuition:          If we store each element as we iterate over the array,
- *                     we can simply check each element as we iterate over the
- *                     array.
+ * Intuition:
+ * If we store each element as we iterate over the array,
+ * we can simply check each element as we iterate over the
+ * array.
  * 
  * Time Complexity:    O(n)
+ * We run through and process each element in the array once since we are using
+ * a hash table to store re-occurrences. However, note that insertions and 
+ * lookups for hash tables have amortized constant time complexities. The
+ * algorithm is therefore linear, as it consists of a for loop that processes 
+ * each element in the array once. So, performance would decrease as n, the size
+ * of the array, decreases. 
  * 
- *                     We run through and process each element in the 
- *                     array once since we are using a hash table to store re-
- *                     occurrences. However, note that insertions and lookups
- *                     for hash tables have amortized constant time 
- *                     complexities. The algorithm is therefore linear, as it
- *                     consists of a `for` loop that processes each element in
- *                     the array once. So, performance would decrease as n, the
- *                     size of the array decreases. 
+ * Space Complexity:   O(n) 
+ * In the worst case, the duplicate element appears twice, with the duplicate 
+ * appearing at array index n - 1. In this case, `seen` will contain n - 1 
+ * distinct values, and will therefore occupy O(n) space.
  * 
- * Space Complexity:   O(n)
- * 
- *                     In the worst case, the duplicate element appears twice,
- *                     with the duplicate appearing at array index n - 1. In
- *                     this case, `seen` will contain n - 1 distinct values,
- *                     and will therefore occupy O(n) space.
- * 
- * 
- * @param {Number[]} _number
+ * @param {Number[]} numbers
  * @return {Number}
  */
-function findDuplicate(_numbers) {
+function findDuplicate2(numbers) {
   // Sets are not constant time! They take up space similar to arrays.
   seen = new Set()
 
-  for (let number in _numbers) {
-    if (seen.has(_numbers[ number ])) {
-      return _numbers[ number ]
+  for (let number in numbers) {
+    if (seen.has(numbers[ number ])) {
+      return numbers[ number ]
     }
 
-    seen.add(_numbers[ number ])
+    seen.add(numbers[ number ])
   }
 }
 
@@ -92,28 +83,24 @@ function findDuplicate(_numbers) {
  * @dev
  * Approach 3: Floyd's Tortoise and Hare (Cycle Detection)
  *
- * Intuition:           The idea is to reduce the problem to Linked List Cycle
- *                      II: https://leetcode.com/problems/linked-list-cycle-ii/solution/
+ * Intuition:
+ * The idea is to reduce the problem to Linked List Cycle II: 
+ * https://leetcode.com/problems/linked-list-cycle-ii/solution/
  *
- *                      "Given a linked list, return the node where the cycle
- *                      begins."
- *
- *                      First, where does the cycle come from? 
- *                      Let's use the function `f(x) = nums[x]` to construct the
- *                      sequence: `x, nums[x], nums[nums[x]], 
- *                      nums[nums[nums[x]]], ...`
- * 
- *                      Each new element in the sequence is an element in nums 
- *                      at the index of 
+ * "Given a linked list, return the node where the cycle begins."
+ * First, where does the cycle come from? Let's use the function 
+ * `f(x) = nums[x]` to construct the sequence: `x, nums[x], nums[nums[x]],  
+ * nums[nums[nums[x]]], ...` 
+ * Each new element in the sequence is an element in nums at the index of 
  * 
  * Time complexiy:      O(n)
  * 
  * Space complexity:    O(1)
  * 
  * 
- * @param {Number[]} _numbers
+ * @param {Number[]} numbers
  * @return {Number}
  */
-function findDuplicate(_numbers) {
+function findDuplicate3(numbers) {
 
 }
--- data-structures-and-algorithms/sliding-window/medium/permutationInString.js
+++ data-structures-and-algorithms/sliding-window/medium/permutationInString.js
@@ -1,58 +1,19 @@
 /**
+ * @dev
+ * 3. Longest Substring Without Repeating Characters
+ *
+ * Difficulty: Medium
+ *
+ * Reference:
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * Approach 1:
+ *
+ * Time complexity:    O()
+ *
+ * Space complexity:   O()
+ *
  * @param {string} s1
  * @param {string} s2
  * @return {boolean}
- * 
- * 
- * @dev
- * 3. Longest Substring Without Repeating Characters
- * 
- * Difficulty:         Medium
- * 
- * Reference:
- * https://leetcode.com/problems/longest-substring-without-repeating-characters/
- * 
- * 
- * Time complexity:    O()
- *
- * 
- * Space complexity:   O()
- * 
- * 
- * @dev
- * ATTEMPT #1
- *
- * Date: 10/01/20
- *
- * Completed (T/F):  F
  */
-function checkInclusion(s1, s2) {
-  // an s1 permutation is any resulting uninterrupted scramble of s1
-
-  // turn s1 into hashMap so that we can quickly check if a char in s2 is also in s1 
-  //let s1Map = new Map(s1) // 18:36 stuck on the right data struc to use to query s1 quickly chars
-
-  // we can use .includes() string method 19:20
-
-
-  // how to keep track of adjacency 9:02
-
-  // run through s2 and process each element
-  for (let end = 0; end < s2.length; end++) {
-    rightChar = s2[ rightChar ]
-
-
-    // while processing an element in s2
-    // use window to keep track of whether chars in s1 are adjacent
-
-    // if current s2 char is in s1 && previous s2 char is in s1, then this is 11:04
-    if (s1.includes(s2[ rightChar ]) && )
-
-
-    // continuous check that every character in a sequence of s2 is somewhere in s1
-
-    // check if s1 permutation in s2
-      // if yes, return true
-      // else, return false
-  }
-}
