9f6065393b435955fcedbf121982f73e35059b54
platocrat <jack.abe.winfield@gmail.com>
2026-08-29 11:42:19 -0500

    nit

--- data-structures-and-algorithms/linked-list/medium/add-two-numbers/addTwoNumbers.ts
+++ data-structures-and-algorithms/linked-list/medium/add-two-numbers/addTwoNumbers.ts
@@ -11,9 +11,11 @@
  */
 
 /**
- * @dev
- * Time complexity: O(max(m, n)), where m and n are the lengths of the two linked lists.
- * Space complexity: O(1), the length of the new linked list is t most max(m, n) + 1, but we don't count the space used for the output linked list. 
+ * @dev This function adds two numbers represented by two linked lists. Each node in the linked list contains a single digit, and the digits are stored in reverse order. The function returns a new linked list representing the sum of the two numbers.
+ * @dev Time and space complexity analysis:
+ * 
+ * - Time complexity: O(max(m, n)), where m and n are the lengths of the two linked lists.
+ * - Space complexity: O(1), the length of the new linked list is t most max(m, n) + 1, but we don't count the space used for the output linked list. 
  * @param l1 
  * @param l2 
  * @returns 
